68e46e04f9dff4df0c87a50f1d7f91e8ca4da3b2
chv <61952908+chaim-chv@users.noreply.github.com>
2020-07-01 21:13:51 +0300

    Add files via upload

---
+++ youtube downloader.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name         youtube downloader
+// @namespace    http://mitmachim.top/user/chv
+// @version      1.0
+// @description  adding download buton to youtube (for netfree users)
+// @author       chv
+// @match        http://www.youtube.com/watch?*
+// @match        https://www.youtube.com/watch?*
+// @grant        none
+// ==/UserScript==
+
+    (function () {
+  const video_id = window.location.search.split("v=")[1];
+  const fix = video_id.indexOf("&");
+  if (fix != -1) {
+    video_id = video_id.substring(0, fix);
+  }
+  const dlink = "https://videownload.ml/download/https://youtu.be/" + video_id;
+  const dbutton = document.createElement("div");
+  dbutton.style.fontSize = "1.35rem"
+  dbutton.innerHTML = `<div id="nfdownload" style="align-items: center; line-height: 2.5"><a style="color :black; text-decoration: none; display: inline-block; border: 2px solid green" href="${dlink}" target="_blank">הורדה בנטפרי</a></div>`;
+  document
+    .querySelector("ytd-menu-renderer #top-level-buttons")
+    .appendChild(dbutton);
+})();
