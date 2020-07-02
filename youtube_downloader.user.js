215912e6054f664164fded3bf247ed239b465140
chv <61952908+chaim-chv@users.noreply.github.com>
2020-07-02 03:01:53 +0300

    make the script run in slow connections

--- youtube_downloader.user.js
+++ youtube_downloader.user.js
@@ -1,15 +1,14 @@
 // ==UserScript==
-// @name         youtube downloader
+// @name         youtube downloader (netfree)
 // @namespace    http://mitmachim.top/user/chv
 // @version      1.0
 // @description  adding download buton to youtube (for netfree users)
 // @author       chv
 // @match        http://www.youtube.com/watch?*
 // @match        https://www.youtube.com/watch?*
-// @grant        none
 // ==/UserScript==
 
-    (function () {
+(function dbutmaker() {
   const video_id = window.location.search.split("v=")[1];
   const fix = video_id.indexOf("&");
   if (fix != -1) {
@@ -19,7 +18,12 @@
   const dbutton = document.createElement("div");
   dbutton.style.fontSize = "1.35rem"
   dbutton.innerHTML = `<div id="nfdownload" style="align-items: center; line-height: 2.5"><a style="color :black; text-decoration: none; display: inline-block; border: 2px solid green" href="${dlink}" target="_blank">הורדה בנטפרי</a></div>`;
-  document
+  var check = document.querySelector("ytd-menu-renderer #top-level-buttons")
+    if (check == null) {
+  setTimeout(() => { document
     .querySelector("ytd-menu-renderer #top-level-buttons")
-    .appendChild(dbutton);
+    .appendChild(dbutton); }, 5000); }
+    else { document
+        .querySelector("ytd-menu-renderer #top-level-buttons")
+        .appendChild(dbutton); };
 })();
