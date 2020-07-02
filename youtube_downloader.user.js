a66b58c32e543a83be6672c9c748ec21af1f0fce
chv <61952908+chaim-chv@users.noreply.github.com>
2020-07-03 00:21:30 +0300

    Update youtube_downloader.user.js

--- youtube_downloader.user.js
+++ youtube_downloader.user.js
@@ -2,49 +2,28 @@
 // @name         youtube downloader (netfree)
 // @namespace    http://mitmachim.top/user/chv
 // @version      1.2
-// @updateURL    https://github.com/chaim-chv/files-sending-for-netfree-users/raw/master/youtube_downloader.user.js
+// @updateURL    https://github.com/chaim-chv/files-sending-for-netfree-users/raw/master/
 // @description  adding download button to youtube (for netfree users)
-// @author       chv
+// @author       chv - netfree user & nefree fan
 // @match        http://www.youtube.com/watch?*
 // @match        https://www.youtube.com/watch?*
 // ==/UserScript==
 
-(function dbutmaker() {
-  const video_id = window.location.search.split("v=")[1];
-  const fix = video_id.indexOf("&");
-  if (fix != -1) {
-    video_id = video_id.substring(0, fix);
-  }
-  const dlink = "https://videownload.ml/download/https://youtu.be/" + video_id;
-  const dbutton = document.createElement("div");
-  dbutton.style.fontSize = "1.35rem";
-  dbutton.innerHTML = `<div id="nfdownload" title="הורדת הסרטון למחשב (דרך האתר videowload.ml)" style="align-items: center; line-height: 2.5"><a style="color :black; text-decoration: none; display: inline-block;" href="${dlink}" target="_blank"><img src="https://github.com/chaim-chv/files-sending-for-netfree-users/raw/master/1.png" width="100" height="38"></a></div>`;
-  var check = document.querySelector("ytd-menu-renderer #top-level-buttons");
-  if (check == null) {
-    setTimeout(() => {
-        if (check == null) {
-            setTimeout(() => {
-              if (check == null) {
-            setTimeout(() => {
-              document
-                .querySelector("ytd-menu-renderer #top-level-buttons")
-                .appendChild(dbutton);
-            }, 3000);
-          } else {
-            document
-              .querySelector("ytd-menu-renderer #top-level-buttons")
-              .appendChild(dbutton);
-             }
-            }, 2000);
-          } else {
-            document
-              .querySelector("ytd-menu-renderer #top-level-buttons")
-              .appendChild(dbutton);
-          }
-    }, 3000);
-  } else {
+const observer = new MutationObserver(() => {
+  if (document.querySelector("ytd-menu-renderer #top-level-buttons")) {
+    observer.disconnect();
+    const video_id = new URLSearchParams(new URL(location).search).get("v");
+    const dlink = "https://videownload.ml/download/https://youtu.be/" + video_id;
+    const dbutton = document.createElement("div");
+    dbutton.style.fontSize = "1.35rem";
+    dbutton.innerHTML = `<div id="nfdownload" title="הורדת הסרטון למחשב (דרך האתר videownload.ml)" style="align-items: center; line-height: 2.5"><a style="color :black; text-decoration: none; display: inline-block;" href="${dlink}" target="_blank"><img src="https://github.com/chaim-chv/files-sending-for-netfree-users/raw/master/1.png" width="100" height="38"></a></div>`;
     document
       .querySelector("ytd-menu-renderer #top-level-buttons")
       .appendChild(dbutton);
   }
-})();
+});
+observer.observe(document, {
+  attributes: true,
+  childList: true,
+  subtree: true,
+});
