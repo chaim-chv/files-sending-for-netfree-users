6a80b60d2cf506f5e184b815a7b574e91448b67d
chv <61952908+chaim-chv@users.noreply.github.com>
2020-07-05 11:35:11 +0300

    Update youtube_downloader.user.js

--- youtube_downloader.user.js
+++ youtube_downloader.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         youtube downloader (netfree)
 // @namespace    http://mitmachim.top/user/chv
-// @version      1.3
+// @version      1.4
 // @updateURL    https://github.com/chaim-chv/files-sending-for-netfree-users/raw/master/
 // @description  adding download button to youtube (for netfree users)
 // @author       chv - netfree user & nefree fan
@@ -16,7 +16,7 @@ const observer = new MutationObserver(() => {
     const dlink = "https://videownload.ml/download/https://youtu.be/" + video_id;
     const dbutton = document.createElement("div");
     dbutton.style.fontSize = "1.35rem";
-    dbutton.innerHTML = `<div id="nfdownload" title="הורדת הסרטון למחשב (דרך האתר videownload.ml)" style="align-items: center; line-height: 2.5"><a style="color :black; text-decoration: none; display: inline-block;" href="${dlink}" target="_blank"><img src="https://github.com/chaim-chv/files-sending-for-netfree-users/raw/master/1.png" width="100" height="38"></a></div>`;
+    dbutton.innerHTML = `<div id="nfdownload" title="הורדת הסרטון למחשב (דרך האתר videownload.ml)" style="align-items: center; line-height: 2.5"><a style="color :black; text-decoration: none; display: inline-block;" href="${dlink}" target="_blank"><img src="http://forum.netfree.link/assets/uploads/files/1593935403429-1200px-youtube_play_buttom_icon_-2013-2017.png" width="35px" height="25px" margin="7px"</a></div>`;
     document
       .querySelector("ytd-menu-renderer #top-level-buttons")
       .appendChild(dbutton);
