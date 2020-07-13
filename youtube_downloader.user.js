1123adcde9f1708976b9e270eb3dc70d278af3fd
chv <61952908+chaim-chv@users.noreply.github.com>
2020-07-13 20:49:37 +0300

    תיקון קטן עקב שינוי כתובת אתר ההורדה בנטפרי

--- youtube_downloader.user.js
+++ youtube_downloader.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         youtube downloader (netfree)
 // @namespace    http://mitmachim.top/user/chv
-// @version      1.4
+// @version      1.5
 // @updateURL    https://github.com/chaim-chv/files-sending-for-netfree-users/raw/master/
 // @description  adding download button to youtube (for netfree users)
 // @author       chv - netfree user & nefree fan
@@ -13,7 +13,7 @@ const observer = new MutationObserver(() => {
   if (document.querySelector("ytd-menu-renderer #top-level-buttons")) {
     observer.disconnect();
     const video_id = new URLSearchParams(new URL(location).search).get("v");
-    const dlink = "https://videownload.ml/download/https://youtu.be/" + video_id;
+    const dlink = "http://yownload.ml/download/https://youtu.be/" + video_id;
     const dbutton = document.createElement("div");
     dbutton.style.fontSize = "1.35rem";
     dbutton.innerHTML = `<div id="nfdownload" title="הורדת הסרטון למחשב (דרך האתר videownload.ml)" style="align-items: center; line-height: 2.5"><a style="color :black; text-decoration: none; display: inline-block;" href="${dlink}" target="_blank"><img src="http://forum.netfree.link/assets/uploads/files/1593935403429-1200px-youtube_play_buttom_icon_-2013-2017.png" width="35px" height="25px" margin="7px"</a></div>`;
