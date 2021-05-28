2318f05c7665dc251349283e8352c06003a97801
chv <61952908+chaim-chv@users.noreply.github.com>
2021-05-28 13:57:07 +0300

    update image URL with secure http path

--- youtube_downloader.user.js
+++ youtube_downloader.user.js
@@ -16,7 +16,7 @@ const observer = new MutationObserver(() => {
     const dlink = "http://yownload.ml/download/https://youtu.be/" + video_id;
     const dbutton = document.createElement("div");
     dbutton.style.fontSize = "1.35rem";
-    dbutton.innerHTML = `<div id="nfdownload" title="הורדת הסרטון למחשב (דרך האתר videownload.ml)" style="align-items: center; line-height: 2.5"><a style="color :black; text-decoration: none; display: inline-block;" href="${dlink}" target="_blank"><img src="http://forum.netfree.link/assets/uploads/files/1593935403429-1200px-youtube_play_buttom_icon_-2013-2017.png" width="35px" height="25px" margin="7px"</a></div>`;
+    dbutton.innerHTML = `<div id="nfdownload" title="הורדת הסרטון למחשב (דרך האתר videownload.ml)" style="align-items: center; line-height: 2.5"><a style="color :black; text-decoration: none; display: inline-block;" href="${dlink}" target="_blank"><img src="https://mitmachim.top/assets/uploads/files/1622199365950-97b5c059-ba0e-484d-8bfa-2befec0f126a-image.png" width="35px" height="25px" margin="7px"</a></div>`;
     document
       .querySelector("ytd-menu-renderer #top-level-buttons")
       .appendChild(dbutton);
